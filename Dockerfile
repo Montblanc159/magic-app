15756da28b89c5af34e60a63c3253d74b06872fd
Guillaume <guillaume.zaugg@gmail.com>
2022-03-16 02:45:53 +0100

    adds live reloading, parcel, docker, docker compose

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM node:12-alpine
 RUN apk add --no-cache python2 g++ make
 WORKDIR /app
 COPY . .
-RUN yarn install --production
+RUN yarn install
 CMD ["yarn", "start"]
 EXPOSE 3000
-
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
+import dotenv from "dotenv"
 import express from "express"
-const app = express()
-const port = 3000
+import path from "path"
+import ejs from "ejs"
+import livereload from "livereload"
+import connectLivereload from "connect-livereload"
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const liveReloadServer = livereload.createServer()
+liveReloadServer.watch(path.join(__dirname, 'public'))
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.use(connectLivereload())
+
+app.use(express.static(__dirname + '/views'))
+
+app.set("views", path.join(__dirname, "views"))
+app.set('view engine', 'ejs');
+
+app.engine('html', ejs.renderFile);
+
+app.get("/", (req, res) => {
+    res.render("index.html")
 })
 
 app.listen(port)
